d5f71fe8a36657980881d3d43f08497a0c8b90db
ioanTeulea <ioanteulea@gmail.com>
2024-05-27 13:02:26 +0300

    Updated project structure and references

---
+++ Tema1/Controllers/ProdusController.cs
@@ -0,0 +1,40 @@
+﻿using Project.Core.Services;
+using Project.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Project.Database.Dtos.Request;
+
+namespace LabProject.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdusController : ControllerBase
+    {
+        private readonly ProdusService _produsService;
+
+        public ProdusController(ProdusService produsService)
+        {
+            _produsService = produsService;
+        }
+
+        [HttpPost]
+        [Route("get-products")]
+        public IActionResult GetAllProduse([FromBody] GetProduseRequest payload )
+        {
+            var produse = _produsService.GetAllProduse(payload);
+            return Ok(produse);
+        }
+
+        [HttpGet]
+        [Route("{produsId}/get-details")]
+        public IActionResult GetProdusById([FromRoute] int produsId)
+        {
+            var produs = _produsService.GetProdusById(produsId);
+            if (produs == null)
+            {
+                return NotFound();
+            }
+            return Ok(produs);
+        }
+    }
+}
---
+++ Tema1/StartupConfig.cs
@@ -0,0 +1,26 @@
+﻿using Project.Core.Services;
+using Project.Database.Context;
+using Project.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Api
+{
+    public static class StartupConfig
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ProdusService>();
+            services.AddScoped<ReviewService>();
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddDbContext<ProjectDbContext>();
+            services.AddScoped<DbContext, ProjectDbContext>();
+
+            services.AddScoped<ProdusRepository>();
+            services.AddScoped<ReviewRepository>();
+          
+        }
+    }
+}
